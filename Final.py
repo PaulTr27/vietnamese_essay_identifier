083444e893655999a1a30e8361fce0bc4a79bc72
PaulTran2734 <84268848+PaulTran2734@users.noreply.github.com>
2022-05-31 19:30:20 +0700

    Update Final.py

--- Final.py
+++ Final.py
@@ -4,8 +4,14 @@ from transformers import AutoModelForSequenceClassification
 import torch
 from annotated_text import annotated_text, annotation
 import os
+import textdistance as td
+import pandas as pd
+import joblib
+import string
 import streamlit as st
 import numpy as np
+import spellcor as sc
+from spellcor import spellcorrect
 @st.cache(allow_output_mutation=True,show_spinner=False)
 def load_model(model_path,tokenizer_path,seg_path):
     segmenter = VnCoreNLP(seg_path, annotators="wseg", max_heap_size='-Xmx500m') 
@@ -14,34 +20,31 @@ def load_model(model_path,tokenizer_path,seg_path):
     return model, tokenizer, segmenter
     # In[6]:
 
- 
-st.title('Testing function app')
 
-
-
-add_selectbox = st.sidebar.selectbox(
-    "What would you like to do ?",
-    ("Use the app", "See the source-code")
-)
-if add_selectbox == 'Use the app':
+def application(): 
     st.write("Welcome to your friendly app")
     cwd = os.getcwd() 
-    seg_path = os.path.join(cwd,'VnCoreNLP-1.1.1.jar')
+    segmenter_path = os.path.join(cwd,'VnCoreNLP\VnCoreNLP-1.1.1.jar')
     with st.spinner(text="Initializing..."):
         classifier, tokenizer, segmenter = load_model("PaulTran/vietnamese_essay_identify",
                                                       "vinai/phobert-base",
-                                                      seg_path)
+                                                      segmenter_path)
     essay = st.text_area('Write your essay here', "Đây là ví dụ",height=600)
     sentences_list = essay.split('.')
 
     submission = st.button('Submit')
     if submission:
         pred_labels_list = []
+        misspelled = []
         label_names = ['Nghị Luận','Biểu cảm','Miêu tả','Tự sự', 'Thuyết minh']
-
         for sentence in sentences_list:
-            sentence = sentence.lower()
+            sentence = sentence.lower().translate(str.maketrans('', '', string.punctuation))
             segment = segmenter.tokenize(sentence)
+            if len(segment) > 0:
+                for word in segment[0]:
+                    
+                    if word not in segmented_vocab and word not in decode_dict.keys():
+                        misspelled.append(word)
             tokenized_txt = tokenizer(sentence,
                                       max_length=tokenizer.model_max_length,
                                       truncation=True,
@@ -60,9 +63,32 @@ if add_selectbox == 'Use the app':
             pred_prob.append((pred_labels_list.count(i)/len(pred_labels_list)))
         pred_label_name = label_names[np.argmax(pred_prob)]
         st.write('Your essay is a {} type'.format(pred_label_name))
-    with st.expander("See more detail here"):
-        txt = annotated_text((essay,"#8ef"))
-        st.write(txt)
+        mis_dict = {}
+        mis_dict["Misspelled Word"] = misspelled
+        fix = []
+        spellcorrecter = spellcorrect(encode_dict,decode_dict,df_vocab,space_svc)
+        for word in misspelled:
+            out = spellcorrecter.spell_correct(word)
+            fix.append(out)
+        mis_dict["Did you mean"] = fix
+        with st.expander("See more detail here"):
+            df_mis = pd.DataFrame(mis_dict)
+            st.dataframe(df_mis)
+            
+st.title('Testing function app')
+df_vocab ,decode_dict,encode_dict, space_svc = sc.get_resources()
+segmented_vocab = df_vocab['Segmented Vocabulary'].to_list()
+vocab = df_vocab['Vocabulary'].to_list()
+telex_vocab = df_vocab['Telex Combination'].to_list()
+add_selectbox = st.sidebar.selectbox(
+    "What would you like to do ?",
+    ("Introduction","Use the app", "See the source-code","How I make this project works")
+)
+
+
+if add_selectbox == 'Use the app':
+    application()
+    
 
 
 
@@ -76,6 +102,7 @@ else:
 
 
 # In[ ]:
+
 
 
 
