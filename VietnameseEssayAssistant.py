7e466bad9aa2ff34c367718470808f7296f24356
PaulTran2734 <84268848+PaulTran2734@users.noreply.github.com>
2022-06-18 09:50:44 +0700

    Update VietnameseEssayAssistant.py

--- VietnameseEssayAssistant.py
+++ VietnameseEssayAssistant.py
@@ -125,7 +125,7 @@ def application():
                     col1.text_area("Fix your essay here",st.session_state.input_text,key = 'input_text',height = 1000)
                     col2.write("Detected mistakes with suggestions")
                     with col2:
-                        st.markdown(f'<p align ="justify">{annotated_text(*ann_list)}</p>',unsafe_allow_html = True)
+                        st.markdown({annotated_text(*ann_list)},unsafe_allow_html = True)
                     p2.write("Click resubmit to submit again after you fixed the mistakes")
                     resub = st.empty()
                     resubmit = resub.button('Resubmit')
